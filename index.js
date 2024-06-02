a8ac1a123f00095242a3f6be1321f7b4747aad56
Emon Ahmed <imonshomon@gmail.com>
2024-06-02 14:01:04 +0600

    done last

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const viewCount = require('./middleware/veiwCount');
+
 const port = process.env.PORT || 5000
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
